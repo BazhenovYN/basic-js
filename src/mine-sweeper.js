b5ed697f80ef13975bfbf95896e92b057a4beb6f
BazhenovYN <bazhenovyn@gmail.com>
2023-04-15 18:04:14 +0300

    feat: done mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
@@ -23,9 +21,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+
+  function checkMines(i, j) {
+    let count = 0;
+    for (let n = -1; n <= 1; n++) {
+      for (let k = -1; k <= 1; k++) {
+        if (n === 0 && k === 0) {
+          continue;
+        }
+        if (i + n >= 0 && j + k >= 0 && i + n < matrix.length && j + k < matrix[i].length) {
+          if (matrix[i + n][j + k]) {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
+  }
+
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      result[i][j] = checkMines(i, j);
+    }
+  }
+  return result;
 }
 
 module.exports = {
