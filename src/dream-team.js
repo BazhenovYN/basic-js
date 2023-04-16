ccbb819195bca1e99d78268301e8c1c3522018f9
BazhenovYN <BazhenovYN@gmail.com>
2023-04-16 23:45:12 +0300

    feat: done dream-team, hanoi-tower, simple-chain

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  
+  const teamName = members
+    .map((name) => {
+      if (typeof name === 'string' && name.length > 0) {
+        return name.trim()[0].toLocaleUpperCase();
+      } else {
+        return '';
+      }
+    })
+    .sort()
+    .join('');
+  if (teamName) {
+    return teamName;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
